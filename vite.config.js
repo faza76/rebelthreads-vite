0adfffae471329fb043e7e09db3afc77cb2652cd
Harnish <harrnish@gmail.com>
2025-05-23 00:48:12 -0400

    Routing config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        work: resolve(__dirname, "work.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
     assetsInclude: [
       "**/*.jpeg",
       "**/*.jpg",
@@ -9,13 +18,6 @@ export default defineConfig({
       "**/*.svg",
       "**/*.gif",
     ],
-
     copyPublicDir: true,
-
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
   },
 });
