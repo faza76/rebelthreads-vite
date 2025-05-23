39d63267693159167afbc54dd51c79e8a72bbaaf
Harnish <harrnish@gmail.com>
2025-05-23 14:16:58 -0400

    Project preview scroll animation

---
+++ js/project.js
@@ -0,0 +1,39 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  ScrollTrigger.create({
+    trigger: ".project-page-whitespace",
+    start: "top bottom",
+    end: "bottom bottom",
+    scrub: 1,
+    onUpdate: (self) => {
+      const projectPreviewWrapper = document.querySelector(
+        ".project-preview-wrapper"
+      );
+      const previewCols = document.querySelectorAll(
+        ".preview-col:not(.main-preview-col)"
+      );
+      const mainPreviewImg = document.querySelector(
+        ".preview-img.main-preview-img img"
+      );
+
+      const previewScreenWidth = window.innerWidth;
+      const previewMaxScale = previewScreenWidth < 900 ? 4 : 2.65;
+
+      const scale = 1 + self.progress * previewMaxScale;
+      const yPreviewColTranslate = self.progress * 300;
+      const mainPreviewImgScale = 2 - self.progress * 0.85;
+
+      projectPreviewWrapper.style.transform = `translate(-50%, -50%) scale(${scale})`;
+
+      previewCols.forEach((previewCol) => {
+        previewCol.style.transform = `translateY(${yPreviewColTranslate}px)`;
+      });
+
+      mainPreviewImg.style.transform = `scale(${mainPreviewImgScale})`;
+    },
+  });
+});
