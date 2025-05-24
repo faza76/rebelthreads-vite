d09f5d28fdf903e81658c7ba6ffc21df212bf262
Harnish <harrnish@gmail.com>
2025-05-23 23:01:03 -0400

    Adding back stats animation to all pages

--- js/about.js
+++ js/about.js
@@ -10,14 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let scrollTriggerInstances = [];
 
   const initAnimations = () => {
-    if (window.innerWidth <= 1000) {
-      scrollTriggerInstances.forEach((instance) => {
-        if (instance) instance.kill();
-      });
-      scrollTriggerInstances = [];
-      return;
-    }
-
     scrollTriggerInstances.forEach((instance) => {
       if (instance) instance.kill();
     });
@@ -26,78 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
     gsap.set([".stats-item-1", ".stats-item-2", ".stats-item-3"], {
       scale: 0,
     });
-
-    const portraitAnimation = gsap.to(".about-hero-portrait", {
-      y: -200,
-      rotation: -25,
-      scrollTrigger: {
-        trigger: ".about-hero",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-      },
-    });
-    scrollTriggerInstances.push(portraitAnimation.scrollTrigger);
-
-    const tag1Animation = gsap.to("#tag-1", {
-      y: -300,
-      rotation: -45,
-      scrollTrigger: {
-        trigger: ".about-copy",
-        start: "top bottom",
-        end: "bottom+=100% top",
-        scrub: 1,
-      },
-    });
-    scrollTriggerInstances.push(tag1Animation.scrollTrigger);
-
-    const tag2Animation = gsap.to("#tag-2", {
-      y: -150,
-      rotation: 70,
-      scrollTrigger: {
-        trigger: ".about-copy",
-        start: "top bottom",
-        end: "bottom+=100% top",
-        scrub: 1,
-      },
-    });
-    scrollTriggerInstances.push(tag2Animation.scrollTrigger);
-
-    const tag3Animation = gsap.to("#tag-3", {
-      y: -400,
-      rotation: 120,
-      scrollTrigger: {
-        trigger: ".about-copy",
-        start: "top bottom",
-        end: "bottom+=100% top",
-        scrub: 1,
-      },
-    });
-    scrollTriggerInstances.push(tag3Animation.scrollTrigger);
-
-    const tag4Animation = gsap.to("#tag-4", {
-      y: -350,
-      rotation: -60,
-      scrollTrigger: {
-        trigger: ".about-copy",
-        start: "top bottom",
-        end: "bottom+=100% top",
-        scrub: 1,
-      },
-    });
-    scrollTriggerInstances.push(tag4Animation.scrollTrigger);
-
-    const tag5Animation = gsap.to("#tag-5", {
-      y: -200,
-      rotation: 100,
-      scrollTrigger: {
-        trigger: ".about-copy",
-        start: "top bottom",
-        end: "bottom+=100% top",
-        scrub: 1,
-      },
-    });
-    scrollTriggerInstances.push(tag5Animation.scrollTrigger);
 
     const statsAnimation = gsap.to(
       [".stats-item-1", ".stats-item-2", ".stats-item-3"],
@@ -114,6 +34,80 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     );
     scrollTriggerInstances.push(statsAnimation.scrollTrigger);
+
+    if (window.innerWidth > 1000) {
+      const portraitAnimation = gsap.to(".about-hero-portrait", {
+        y: -200,
+        rotation: -25,
+        scrollTrigger: {
+          trigger: ".about-hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: 1,
+        },
+      });
+      scrollTriggerInstances.push(portraitAnimation.scrollTrigger);
+
+      const tag1Animation = gsap.to("#tag-1", {
+        y: -300,
+        rotation: -45,
+        scrollTrigger: {
+          trigger: ".about-copy",
+          start: "top bottom",
+          end: "bottom+=100% top",
+          scrub: 1,
+        },
+      });
+      scrollTriggerInstances.push(tag1Animation.scrollTrigger);
+
+      const tag2Animation = gsap.to("#tag-2", {
+        y: -150,
+        rotation: 70,
+        scrollTrigger: {
+          trigger: ".about-copy",
+          start: "top bottom",
+          end: "bottom+=100% top",
+          scrub: 1,
+        },
+      });
+      scrollTriggerInstances.push(tag2Animation.scrollTrigger);
+
+      const tag3Animation = gsap.to("#tag-3", {
+        y: -400,
+        rotation: 120,
+        scrollTrigger: {
+          trigger: ".about-copy",
+          start: "top bottom",
+          end: "bottom+=100% top",
+          scrub: 1,
+        },
+      });
+      scrollTriggerInstances.push(tag3Animation.scrollTrigger);
+
+      const tag4Animation = gsap.to("#tag-4", {
+        y: -350,
+        rotation: -60,
+        scrollTrigger: {
+          trigger: ".about-copy",
+          start: "top bottom",
+          end: "bottom+=100% top",
+          scrub: 1,
+        },
+      });
+      scrollTriggerInstances.push(tag4Animation.scrollTrigger);
+
+      const tag5Animation = gsap.to("#tag-5", {
+        y: -200,
+        rotation: 100,
+        scrollTrigger: {
+          trigger: ".about-copy",
+          start: "top bottom",
+          end: "bottom+=100% top",
+          scrub: 1,
+        },
+      });
+      scrollTriggerInstances.push(tag5Animation.scrollTrigger);
+    }
   };
 
   initAnimations();
