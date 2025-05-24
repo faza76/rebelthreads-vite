9e1ea81f13492a3e551d3727be7de529a01eaaf2
Harnish <harrnish@gmail.com>
2025-05-24 00:12:12 -0400

    Project page header animation

--- js/project.js
+++ js/project.js
@@ -1,11 +1,82 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/all";
 
 document.addEventListener("DOMContentLoaded", () => {
   const isProjectPage = document.querySelector(".page.project-page");
   if (!isProjectPage) return;
 
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger, SplitText);
+
+  const initHeroAnimations = () => {
+    const heroTitle = SplitText.create(".project-hero-header-h1 h1", {
+      type: "lines",
+      mask: "lines",
+    });
+
+    const projectTags = SplitText.create(".project-tags p", {
+      type: "lines",
+      mask: "lines",
+    });
+
+    const heroDescription = SplitText.create(".project-hero-description p", {
+      type: "lines",
+      mask: "lines",
+    });
+
+    gsap.set([heroTitle.lines, projectTags.lines, heroDescription.lines], {
+      position: "relative",
+      y: "120%",
+      willChange: "transform",
+    });
+
+    gsap.set(".project-hero-header-h1 img", {
+      scale: 0,
+      willChange: "transform",
+    });
+
+    const heroTl = gsap.timeline({ delay: 0.85 });
+
+    heroTl.to(heroTitle.lines, {
+      y: "0%",
+      duration: 1,
+      ease: "power4.out",
+    });
+
+    heroTl.to(
+      ".project-hero-header-h1 img",
+      {
+        scale: 1,
+        duration: 1,
+        ease: "power4.out",
+      },
+      "-=1"
+    );
+
+    heroTl.to(
+      projectTags.lines,
+      {
+        y: "0%",
+        duration: 1,
+        ease: "power4.out",
+        stagger: 0.1,
+      },
+      "-=0.9"
+    );
+
+    heroTl.to(
+      heroDescription.lines,
+      {
+        y: "0%",
+        duration: 1,
+        ease: "power4.out",
+        stagger: 0.1,
+      },
+      "-=0.9"
+    );
+  };
+
+  initHeroAnimations();
 
   ScrollTrigger.create({
     trigger: ".project-page-whitespace",
